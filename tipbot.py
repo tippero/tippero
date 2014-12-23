327a3efa7fac677352bb64bba45c6861d1259cfc
moneromooo <moneromoo@nowhere.nowhere.nowhere>
2014-12-23 10:47:56 +0000

    Licence as GPLv2+.

--- tipbot.py
+++ tipbot.py
@@ -4,6 +4,11 @@
 # Copyright 2014 moneromooo
 # Inspired by "Simple Python IRC bot" by berend
 #
+# The Cryptonote tipbot is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
 
 import socket
 import select
